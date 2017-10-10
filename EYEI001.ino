f9f26a86b564716c1e42ba4ac0035d584451b942
Kim Ngan Nguyen <ngan@vlan-2637-10-16-152-82.student.wireless.sydney.edu.au>
2017-10-10 13:42:35 +1100

    remove on/off push button & added neoPixel

--- EYEI001.ino
+++ EYEI001.ino
@@ -1,73 +1,42 @@
 #include <SPI.h>
 #include <SdFat.h>
 #include <SFEMP3Shield.h>
+#include <TimeLib.h>
+
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 15
 
 //libaries for the mp3 shield
 SdFat sd;
 SFEMP3Shield MP3player;
 
-//set values of led rgb pins
-
-int redPin = 12;
-int greenPin = 5;
-int bluePin = 3;
-
-int pushButton = 0;
-
-int val = 0;
-int deviceOn = false;
+//Adafruit NeoPixel
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-
-
-  
   //start the shield
   sd.begin(SD_SEL, SPI_HALF_SPEED);
   MP3player.begin();
 
-  //setup led rgb pins
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  setColor(0,0,0);
-  pinMode(pushButton, INPUT_PULLUP);
+  //NeoPixel
+  strip.begin();
+  strip.setBrightness(30); //adjust brightness here
+  strip.show();
   
   Serial.begin(9600);
 }
 
 //do something else now
 void loop() {
-  val = digitalRead(pushButton);
-
-  Serial.println(deviceOn);
-  if(deviceOn){
-    
-  }else{
-    
-  }
-  
-  if(val == LOW){
-    //button is pressed  
-    if(deviceOn){
-      deviceOn = false;
-      Serial.println("device turned off");
-    }else{
-      deviceOn = true;
-      Serial.println("device turned on");
-    }
-    delay(1000);
-  }else{
-    //button releases
-  }
+  MP3player.playTrack(1);
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
 }
 
-void setColor(int red, int green, int blue){
-  red = 255 - red;
-  green = 255 - green;
-  blue = 255 - blue;
-
-  digitalWrite(redPin, red);
-  digitalWrite(greenPin, green);
-  digitalWrite(bluePin, blue);
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
 }
-
